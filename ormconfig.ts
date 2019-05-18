19916dac07741aa58e8869cc8f65eb3969eca236
zeromake <a390720046@gmail.com>
2019-05-18 11:48:11 +0800

    feat: update orm config

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,23 @@
-import local from './config/config.local';
-import prod from './config/config.prod';
+import * as fs from 'fs';
 import unittest from './config/config.unittest';
 
 const env = process.env.EGG_SERVER_ENV || 'local';
 
 const configMap = {
-    local,
-    prod,
     unittest,
 };
+[
+    "local",
+    "prod",
+].forEach(env => {
+    const path = `./config/config.${env}.ts`;
+    if (fs.existsSync(path)) {
+        let cfg = require(path);
+        cfg = cfg.default || cfg;
+        configMap[env] = cfg;
+    }
+});
+
 const config = configMap[env]({});
 module.exports = {
     ...config.typeorm.connection,
